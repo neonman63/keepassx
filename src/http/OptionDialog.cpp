078d5693f02f4ee0d3e2f709ee7911e949dee85e
Eugene San (eugenesan) <eugenesan@gmail.com>
2015-07-10 12:50:22 -0400

    License http plugin source code

--- src/http/OptionDialog.cpp
+++ src/http/OptionDialog.cpp
@@ -1,14 +1,18 @@
-/**
- ***************************************************************************
- * @file OptionDialog.cpp
+/*
+ *  Copyright (C) 2013 Francois Ferrand <thetypz@gmail.com>
  *
- * @brief
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 2 or (at your option)
+ *  version 3 of the License.
  *
- * Copyright (C) 2013
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
  *
- * @author	Francois Ferrand
- * @date	4/2013
- ***************************************************************************
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include "OptionDialog.h"
